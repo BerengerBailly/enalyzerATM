b3bea93b3c859b5908a630f0313edcf54c01f823
BERENGER <berenger.bailly@gmail.com>
2018-03-06 19:11:30 -0500

    Creation branche master

---
+++ enalyzerATM/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace enalyzerATM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.AmountTitle = "Select amount";
+            return View();
+        }
+    }
+}
---
+++ enalyzerATM/Scripts/enalyzerATM.js
@@ -0,0 +1,28 @@
+﻿$("#sectionHome").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#sectionTouchPad").offset().top
+    },
+    'slow');
+});
+
+$("#amountButton").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#sectionResult").offset().top
+    },
+        'slow');
+});
+
+$("#backArrow").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#sectionTouchPad").offset().top
+    },
+        'slow');
+});
+
+// TODO : See why scroll not fired
+// $("#appHome").on('scroll', function () {
+//    $('html,body').animate({
+//        scrollTop: $("#appTouchPad").offset().top
+//    },
+//        'slow');
+//});
